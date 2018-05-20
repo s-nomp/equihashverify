0ce03bc647d1d12bef00afb7c34eec663ca7d098
nick <33939323+NickRHill@users.noreply.github.com>
2018-05-20 01:22:31 +0100

    update called function

--- equihashverify.cc
+++ equihashverify.cc
@@ -19,7 +19,7 @@ int verifyEH(const char *hdr, const std::vector<unsigned char> &soln){
 
   crypto_generichash_blake2b_update(&state, (const unsigned char*)hdr, 140);
 
-  bool isValid = Eh200_9.IsValidSolution(state, soln);
+  bool isValid = Eh144_5.IsValidSolution(state, soln);
   
   return isValid;
 }
