0f6c9997abfe6df7b3e099fdb8c182887b5f9cd4
nettts <40123869+nettts@users.noreply.github.com>
2018-08-05 19:28:47 -0400

    Added missing perslization string

--- crypto/equihash.cpp
+++ crypto/equihash.cpp
@@ -819,7 +819,7 @@ template bool Equihash<48,5>::OptimisedSolve(const eh_HashState& base_state,
 template bool Equihash<48,5>::IsValidSolution(const eh_HashState& base_state, std::vector<unsigned char> soln);
 
 // Explicit instantiations for Equihash<144,5>
-template int Equihash<144,5>::InitialiseState(eh_HashState& base_state);
+template int Equihash<144,5>::InitialiseState(eh_HashState& base_state, const char* personalizationString);
 #ifdef ENABLE_MINING
 template bool Equihash<144,5>::BasicSolve(const eh_HashState& base_state,
                                           const std::function<bool(std::vector<unsigned char>)> validBlock,
@@ -831,7 +831,7 @@ template bool Equihash<144,5>::OptimisedSolve(const eh_HashState& base_state,
 template bool Equihash<144,5>::IsValidSolution(const eh_HashState& base_state, std::vector<unsigned char> soln);
 
 // Explicit instantiations for Equihash<192,7>
-template int Equihash<192,7>::InitialiseState(eh_HashState& base_state);
+template int Equihash<192,7>::InitialiseState(eh_HashState& base_state, const char* personalizationString);
 #ifdef ENABLE_MINING
 template bool Equihash<192,7>::BasicSolve(const eh_HashState& base_state,
                                          const std::function<bool(std::vector<unsigned char>)> validBlock,
